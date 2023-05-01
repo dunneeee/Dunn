029893532a07210e10e577e96eabba39a2d11c94
admin Dung <corepper03@gmail.com>
2023-05-01 09:19:28 +0700

    fix rename

--- src/commands/rename.js
+++ src/commands/rename.js
@@ -23,7 +23,7 @@ class Rename extends Command {
         if(type == 'me') {
             if(args.length === 1) return "Vui lòng nhập tên mới"
             const name = args.slice(1).join(" ")
-            await this.api.setTitle(name, event.threadID)
+            await this.api.changeNickname(name, event.threadID, event.senderID)
             return "Đã đổi tên của bạn thành " + name
         }
         const userids = Object.keys(event.mentions)
@@ -31,7 +31,7 @@ class Rename extends Command {
             if(args.length === 0) return this.usage.replace("<prefix>", await this.hook.getPrefix(event))
             const name = args.join(" ")
             await this.api.setTitle(name, event.threadID)
-            return "Đã đổi tên của bạn thành " + name
+            return "Đã đổi tên nhóm bạn thành " + name
         }
         const preIndex = args.findIndex(e => e== ":")
         if(preIndex === -1) return "Có vẻ bạn nhập sai cú pháp: @tag : <Biệt danh>"
